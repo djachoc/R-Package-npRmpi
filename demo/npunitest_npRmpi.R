af4da0fd803d8a8e57da66598ce8caf3c8451b90
Jeffrey Racine <racinej@mcmaster.ca>
2010-05-14 00:37:39 +0000

    .

---
+++ demo/npunitest_npRmpi.R
@@ -0,0 +1,46 @@
+## Make sure you have the .Rprofile file from npRmpi/inst/ in your
+## current directory or home directory. It is necessary.
+
+## To run this on systems with OPENMPI installed and working, try
+## mpirun -np 2 R CMD BATCH npudensml_npRmpi. Check the time in the
+## output file foo.Rout (the name of this file with extension .Rout),
+## then try with, say, 4 processors and compare run time.
+
+## Initialize master and slaves.
+
+mpi.bcast.cmd(np.mpi.initialize(),
+              caller.execute=TRUE)
+
+## Turn off progress i/o as this clutters the output file (if you want
+## to see search progress you can comment out this command)
+
+mpi.bcast.cmd(options(np.messages=FALSE),
+              caller.execute=TRUE)
+
+## Generate some data and broadcast it to all slaves (it will be known
+## to the master node)
+
+set.seed(42)
+
+n <- 1000
+
+x <- rnorm(n)
+y <- rnorm(n)
+
+mpi.bcast.Robj2slave(x)
+mpi.bcast.Robj2slave(y)
+
+## A simple example
+
+t <- system.time(mpi.bcast.cmd(output <- npunitest(x,y,bootstrap=TRUE),
+                               caller.execute=TRUE))
+
+output
+
+## Clean up properly then quit()
+
+mpi.close.Rslaves()
+
+cat("Elapsed time =", t[3], "\n")
+
+mpi.quit()
---
+++ demo/npunitest_serial.R
@@ -0,0 +1,24 @@
+## This is the serial version of npudensml_npRmpi.R for comparison
+## purposes (bandwidth ought to be identical, timing may
+## differ). Study the differences between this file and its MPI
+## counterpart for insight about your own problems.
+
+library(np)
+options(np.messages=FALSE)
+
+## Generate some data
+
+set.seed(42)
+
+n <- 1000
+
+x <- rnorm(n)
+y <- rnorm(n)
+
+## A simple example
+
+t <- system.time(output <- npunitest(x,y,bootstrap=TRUE))
+
+output
+
+cat("Elapsed time =", t[3], "\n")
