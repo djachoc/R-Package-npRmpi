2d4394cb13a3fa23ce1a4a0aa742d30d3996194a
Jeffrey Racine <racinej@mcmaster.ca>
2010-05-18 21:07:47 +0000

    Spawning works!

---
+++ demo/interactive_Rterm.R
@@ -0,0 +1,58 @@
+## This code can be run in real time inside the R terminal/console.
+## It does not require that you have the .Rprofile file from
+## npRmpi/inst/ in your current directory or home directory as we can
+## load the npRmpi package in the standard manner.
+
+library(npRmpi)
+
+## Now we can spawn our slaves from within the R terminal manually. On
+## a two core desktop we need an additional slave on top of the master
+## node to allow both cores to run simultaneously.
+
+mpi.spawn.Rslaves(nslaves=1)
+
+## Initialize master and slaves.
+
+mpi.bcast.cmd(np.mpi.initialize(),
+              caller.execute=TRUE)
+
+## Turn off progress i/o as this clutters the output file (if you want
+## to see search progress you can comment out this command)
+
+## Load your data and broadcast it to all slave nodes
+
+library(MASS)
+
+mpi.bcast.cmd(set.seed(42),
+              caller.execute=TRUE)
+
+n <- 500
+
+rho <- 0.25
+mu <- c(0,0)
+Sigma <- matrix(c(1,rho,rho,1),2,2)
+data <- mvrnorm(n=n, mu, Sigma)
+mydat <- data.frame(x=data[,2],y=data[,1])
+
+mpi.bcast.Robj2slave(mydat)
+
+## A conditional density estimation example. 
+
+t <- system.time(mpi.bcast.cmd(bw <- npcdensbw(y~x,
+                                               bwmethod="cv.ml",
+                                               data=mydat),
+                               caller.execute=TRUE))
+
+summary(bw)
+
+t <- t + system.time(mpi.bcast.cmd(model <- npcdens(bws=bw),
+                                   caller.execute=TRUE))
+
+summary(model)
+
+cat("Elapsed time =", t[3], "\n")
+
+## Clean up properly then quit()
+
+mpi.bcast.cmd(mpi.quit(),
+              caller.execute=TRUE)
